70f1dedaab73daa8397915f17e1d9ff47141b1e9
Ben Hagan <BenHagan@users.noreply.github.com>
2016-08-12 23:41:21 -0500

    Non existant account user story

--- scripts.js
+++ scripts.js
@@ -18,7 +18,8 @@ $(document).ready(function () {
 
 function fetchTwitchJSON(callback) {
     var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
-                      "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+                      "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas",
+                     "comster404", "brunofin"];
     var twitchData = [];
 
   for(i=0;i<usernames.length;i++) {
@@ -33,6 +34,9 @@ function jsonCalls(i, callback) {
       $.getJSON("https://api.twitch.tv/kraken/streams/" + i +
                 "?callback=?", function(stream) {
                   //console.log(stream);
+                  if (channel.status === 422) {
+                    channel.display_name = i;
+                  }
                   callback(channel, stream);
       });
   });
@@ -43,9 +47,13 @@ function drawInterface(channel, stream) {
     var logo = channel.logo;
     var indicatorClass = "indicator-offline";
     var streamingTitle = "";
-    if(stream.stream !== null) {
+    if(stream.stream != null) {
       indicatorClass = "indicator-online";
       streamingTitle = stream.stream.channel.status;
+    }
+    if(channel.status === 422) {
+      streamingTitle = "Account Closed";
+      logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Question_Mark.svg/2000px-Question_Mark.svg.png";
     }
     var html = "<a href='" + channel.url +"' target='_blank'><div class='row user'><img class='logo' src='" + logo + "'><h3>" + displayName + "</h3>" + "<div class='pull-right " + indicatorClass + "'></div><div class='status'>" + streamingTitle  + "</div></div></a>";
     $("#content").append(html);
